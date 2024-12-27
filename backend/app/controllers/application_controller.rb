1e8cd8f1321460b4476997aff15f6ce15e759a07
Itsuki54 <5shi.icyu@gmail.com>
2024-12-27 12:13:37 +0900

    fin:docker

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def health
+    render json: { message: "I'm alive" }
+  end
+  def index
+    render json: { message: "Welcome to the API" }
+  end
+  def not_found
+    render json: { error: 'not_found' }
+  end
 end
